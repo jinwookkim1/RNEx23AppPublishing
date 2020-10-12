54f542d038b1215be4545811dc77bb6788c05650
Wook <refoz0721@gmail.com>
2020-10-12 13:40:16 +0900

    ex23

---
+++ Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default Main=()=>{
+    return(
+        <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+            <Text>React Native Publishing App</Text>
+        </View>
+    );
+}
+
+
